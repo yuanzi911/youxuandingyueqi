2385d9beed988f670aa76cdfa17a4b05e09bf5bf
yuanzi911 <162786720+yuanzi911@users.noreply.github.com>
2024-05-02 07:32:27 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -33,7 +33,7 @@ let addressesnotlsapi = [
 
 let DLS = 8;//速度下限
 let addressescsv = [
-		//'https://raw.githubusercontent.com/yuanzi911/yxip1/main/result.csv', //iptest测速结果文件。
+		//'https://raw.githubusercontent.com/yuanzi911/yxip1/main/result.csv', 
 ];
 
 let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
